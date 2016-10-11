5bbdd3e9e6eb9fe7806dc5864d24aa4904fbc400
alsarse <alsarse@gmail.com>
2016-10-11 11:41:42 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,15 @@
+<html>
+<?php 
+
+print "<div id=foot>
+<ul id='navlistfoot'>
+<h3>Opciones</h3>
+<li class='lifoot'>Opcion 1</li>
+<li class='lifoot'>Opcion 2</li>
+<li class='lifoot'>Opcion 3</li>
+</ul>
+<h2 id=logodrcha>LOGO</h2>
+</div>";
+
+ ?>
+ </html>
---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<?php 
+
+include("header.php");
+
+print "<div>
+		<div id=seccion1 class=container-fluid>
+			
+		</div>
+		<div id=seccion2 class=container-fluid>
+		
+		</div>
+		<div id=seccion3 class=container-fluid>
+			
+		</div>
+		<div id=seccion4 class=container-fluid>
+		
+		</div>
+		<div id=seccion5 class=container-fluid>
+		
+		</div>
+</div>";
+
+include("footer.php");
+
+ ?>
+ </html>
